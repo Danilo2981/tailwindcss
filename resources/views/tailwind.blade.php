ab31e1b6d1b41f284a2c5de1577a7999e443e587
Danilo <danilo.vega@hotmail.es>
2021-02-01 16:12:56 -0500

    instalacion tailwind

---
+++ resources/views/tailwind.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Tailwind --->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>Tailwind</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2>Tailwind</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe natus vero officiis facere nostrum voluptatem adipisci, veniam quaerat culpa laudantium, doloremque dignissimos modi sapiente architecto dolorum odit. Excepturi, officiis laboriosam.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
